658775b61e7c79f29c7ef6f86c70ea7df183f9c8
Juan David Nunez <juandavidnh93@gmail.com>
2019-11-01 16:17:30 -0500

    console print image urls

---
+++ index.js
@@ -0,0 +1,36 @@
+function generateURL(numberOfDogs){
+    console.log(`https://dog.ceo/api/breeds/image/random/${numberOfDogs}`);
+    return `https://dog.ceo/api/breeds/image/random/${numberOfDogs}`;
+
+}
+
+function listenToNumber(){
+    let numberDogs = $('form').find('input[type="number"]').val();
+   return generateURL(numberDogs);
+
+}
+
+
+function displayResults(responseJson) {
+    console.log(responseJson);
+}
+
+function getDogImages(URL){
+    fetch(URL)
+    .then(response => response.json())
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => alert('Something went wrong'));
+}
+
+
+
+function watchForm(){
+    $('form').on('click', 'button', function(e){
+        e.preventDefault();
+        let apiURL = listenToNumber();
+        getDogImages(apiURL);
+    })
+}
+
+
+$(watchForm);
